832d0dfd7492b466d9deae1b2b4c0e8d0744f36e
Elson Santos <45674699+elsonss1988@users.noreply.github.com>
2022-02-25 14:36:52 -0300

    Add: Login Screen

--- app/src/main/java/com/example/hello/MainActivity.java
+++ app/src/main/java/com/example/hello/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.hello;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.example.hello.common.Common;
 
 public class MainActivity extends AppCompatActivity {
 
     String name = "Elson";
+    int idade=0;
     TextView txtHello;
+    TextView txtIdade;
+    Button btnAction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +26,18 @@ public class MainActivity extends AppCompatActivity {
         txtHello = findViewById(R.id.txt_hello);
         txtHello.setText(name);
 
+        txtIdade = findViewById(R.id.txt_idade);
+        txtIdade.setText(idade);
+
+        btnAction=findViewById(R.id.btn_action1);
+        btnAction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Common.globalName = name;
+                Intent clikOne = new Intent(MainActivity.this,HomeActivity.class);
+                startActivity(clikOne);
+                finish();
+            }
+        });
     }
 }
